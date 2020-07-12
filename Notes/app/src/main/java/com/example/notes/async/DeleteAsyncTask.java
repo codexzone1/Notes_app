87b193365fec26527f3d1a8ee776bacc7d3e04a9
Shubham Kumar <shubhamkumar65553@gmail.com>
2020-07-12 19:37:53 +0530

    Initial commit

---
+++ Notes/app/src/main/java/com/example/notes/async/DeleteAsyncTask.java
@@ -0,0 +1,19 @@
+package com.example.notes.async;
+
+import android.os.AsyncTask;
+
+import com.example.notes.models.Note;
+import com.example.notes.persistence.NoteDao;
+
+public class DeleteAsyncTask extends AsyncTask<Note, Void, Void> {
+    private NoteDao mNoteDao;
+
+    public DeleteAsyncTask(NoteDao dao) {
+        mNoteDao = dao;
+    }
+    @Override
+    protected Void doInBackground(Note... notes) {
+        mNoteDao.delete(notes);
+        return null;
+    }
+}
---
+++ Notes/app/src/main/java/com/example/notes/persistence/NoteRepository.java
@@ -0,0 +1,38 @@
+package com.example.notes.persistence;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import com.example.notes.async.DeleteAsyncTask;
+import com.example.notes.async.InsertAsyncTask;
+import com.example.notes.async.UpdateAsyncTask;
+import com.example.notes.models.Note;
+
+public class NoteRepository {
+
+    private NoteDatabase mNoteDatabase;
+
+    public NoteRepository(Context context) {
+        mNoteDatabase = NoteDatabase.getInstance(context);
+    }
+
+    public void insertNoteTask(Note note){
+        new InsertAsyncTask(mNoteDatabase.getNoteDao()).execute(note);
+    }
+
+    public void updateNoteTask(Note note){
+        new UpdateAsyncTask(mNoteDatabase.getNoteDao()).execute(note);
+    }
+
+    public LiveData<List<Note>> retrieveNotesTask() {
+
+        return mNoteDatabase.getNoteDao().getNotes();
+    }
+
+    public void deleteNoteTask(Note note){
+        new DeleteAsyncTask(mNoteDatabase.getNoteDao()).execute(note);
+    }
+}
